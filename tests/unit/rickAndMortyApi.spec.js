d4c54c37c7ae314f476702a74fa2141a1d715800
PestoP <zanardo.florian@gmail.com>
2019-09-27 10:50:05 +0200

    test(fetch characters): initialize fetch character action's test

--- tests/unit/rickAndMortyApi.spec.js
+++ tests/unit/rickAndMortyApi.spec.js
@@ -14,7 +14,23 @@ describe('example', () => {
     expect(characters).not.toBeNull()
     expect(characters).toBeDefined()
     expect(characters).toHaveLength(20)
-    expect(characters).toHaveLength(20)
     expect(characters[0]).toHaveProperty('id', 'name', 'origin', 'status', 'species', 'gender', 'location', 'image', 'episode', 'url', 'created');
+  })
+})
+
+describe('example', () => {
+  it('fecth Only one Character', async () => {
+    let character
+
+    // mock the commit event to get the result in character variable
+    let mockCommit = (state, payload) => {
+      character = payload
+    }
+
+    await charactersModule.actions.fetchCharacter({ commit: mockCommit })
+
+    expect(character).not.toBeNull()
+    expect(character).toBeDefined()
+    expect(character).toHaveProperty('id', 'name', 'origin', 'status', 'species', 'gender', 'location', 'image', 'episode', 'url', 'created');
   })
 })
