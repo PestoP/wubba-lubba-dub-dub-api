a30d687aebf106e0ff9c6e72c4cc09aa43a96aef
PestoP <zanardo.florian@gmail.com>
2019-09-27 11:04:09 +0200

    feat(character view): put semantic html

--- src/components/Character.vue
+++ src/components/Character.vue
@@ -1,5 +1,20 @@
 <template>
-  <div>{{this.character}}</div>
+  <section class="character">
+    <header class="character-header">
+      <img :src='this.character.image' class="character-image"/>
+      <h2 class="character-name">{{this.character.name}}</h2>
+    </header>
+    <div class="character-body">
+      <p class="character-name">{{this.character.name}}</p>
+      <p class="character-status">{{this.character.status}}</p>
+      <p class="character-species">{{this.character.species}}</p>
+      <p class="character-type">{{this.character.type}}</p>
+      <p class="character-gender">{{this.character.gender}}</p>
+      <p class="character-origin">{{this.character.origin}}</p>
+      <p class="character-episodes">{{this.character.episode}}</p>
+      <p class="character-url">{{this.character.url}}</p>
+    </div>
+  </section>
 </template>
 
 <script>
