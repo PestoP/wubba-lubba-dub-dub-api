53dc477c4a76c1fa5980255cd26fb18bd8bd9967
PestoP <zanardo.florian@gmail.com>
2019-09-27 11:34:59 +0200

    refactor(characters style): remove useless background color
    Because overwritten few line below

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -46,7 +46,6 @@ export default {
   grid-gap: 2rem;
 
   .card {
-    background-color: #fff;
     border: solid black 1px;
     border-radius: 10px;
     box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
