f7029f4a3778c65b5b7c44971966fedb566caaf1
PestoP <zanardo.florian@gmail.com>
2019-09-27 09:31:08 +0200

    feat(characters view): add component semantic html

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -1,5 +1,22 @@
 <template>
-  <div>Initialized</div>
+  <div class=cards>
+    <div class="card" v-for="(n, index) in 10" :key='index'>
+      <img class='image-card' src='' alt="background image portrait for the character card">
+      <div class='description-card'>
+        <h2>
+          name of character
+        </h2>
+        <p class="origin-name">
+          <span>
+            Origin's Characters : origin earth
+          </span>
+        </p>
+        <button class="button-explore">
+          Explore
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
