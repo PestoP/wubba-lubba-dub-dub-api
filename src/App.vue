47b625075349a71cd5ca2112624037b5bdccb62f
PestoP <zanardo.florian@gmail.com>
2019-09-27 09:49:34 +0200

    refactor(button): remove css default style of buttons

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,15 @@ html, body {
   margin-right: auto;
   padding: 0 1rem;
 }
+
+// remove button default style
+button {
+  background: none;
+  color: inherit;
+  border: none;
+  padding: 0;
+  font: inherit;
+  cursor: pointer;
+  outline: inherit;
+}
 </style>
