c744e7f27ceaaa0d1f47a1612de88f1b7b5785a9
PestoP <zanardo.florian@gmail.com>
2019-09-27 09:38:20 +0200

    feat(characters card): first layout of cards

--- src/components/Characters.vue
+++ src/components/Characters.vue
@@ -25,11 +25,27 @@ export default {
 }
 </script>
 
-<style>
+<style lang='scss' scoped>
 .cards {
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(16rem, 1fr));
   grid-template-rows: auto;
   grid-gap: 1rem;
+
+  .card {
+    background-color: #fff;
+    border: solid black 1px;
+    border-radius: 10px;
+    box-shadow: 0px 3px 1px -2px rgba(0, 0, 0, 0.2), 0px 2px 2px 0px rgba(0, 0, 0, 0.14), 0px 1px 5px 0px rgba(0, 0, 0, 0.12);
+    background-color: #e8e8e8;
+
+    .image-card {
+      height: 300px;
+    }
+
+    .description-card {
+      height: 150px;
+    }
+  }
 }
 </style>
