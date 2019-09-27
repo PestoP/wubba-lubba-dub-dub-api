74b4dfa4bb6cda22750c7ac239a4d04a8a34a5a7
PestoP <zanardo.florian@gmail.com>
2019-09-27 12:19:30 +0200

    feat(header): put a little header to make more spaces
    and better understanding of what this is about

---
+++ src/views/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='header'>
+    <h1> Rick and Morty's Character By PestoP</h1>
+  </div>
+</template>
+
+<style lang='scss' scoped>
+
+.header{
+  text-align: center;
+  color: #0c93a8;
+}
+
+</style>
