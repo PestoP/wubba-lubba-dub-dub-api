1833e0a963f5c322c90abea48fbea8b628103fda
PestoP <zanardo.florian@gmail.com>
2019-09-27 09:25:53 +0200

    feat(css to begin): put a default style in place

--- src/App.vue
+++ src/App.vue
@@ -3,4 +3,15 @@
 </template>
 
 <style lang="scss">
+html, body {
+  margin: 0;
+  margin-bottom: 20px;
+}
+
+#app {
+  max-width: 65rem;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 0 1rem;
+}
 </style>
