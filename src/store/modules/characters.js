bce09e0fff449d39fe722778a923d3c763a64d7d
PestoP <zanardo.florian@gmail.com>
2019-09-27 10:37:15 +0200

    feat(fetch characters): fetch characters data and commit them

--- src/store/modules/characters.js
+++ src/store/modules/characters.js
@@ -1,13 +1,19 @@
+import axios from 'axios'
+
 export default {
   state: {
+    characters: []
   },
   getters: {
+    allCharacters: (state) => state.characters
   },
   mutations: {
+    setCharacters: (state, characters) => (state.characters = characters)
   },
   actions: {
-    async fetchCharacters () {
-      return null
+    async fetchCharacters ({ commit }) {
+      const response = await axios.get(`https://rickandmortyapi.com/api/character?page=${Math.round((Math.random() * 10))}`)
+      commit('setCharacters', response.data.results)
     }
   }
 }
